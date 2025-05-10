03e15134d79e951ffdb400b689ab735a98414382
Zeutyz <gustavoreduaoliveira@gmail.com>
2025-05-10 20:31:29 -0300

    feat: set up basic user endpoint

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
+
+const users = [];
+
+app.post('/usuarios', (req, res) => {
+    console.log(req.body);
+    users.push(req.body);
+    res.status(201).json(req.body);
+
+});
 
 app.get('/usuarios', (req, res) => {
-    res.send('ok deu bom');
+    res.json(users).status(200);
+    res.send('ok, tudo certo');
 });
 
 app.listen(3000);
